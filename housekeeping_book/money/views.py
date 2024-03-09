a3dcc2489fb4531e4841608e22f8b6803c804a19
674nam <moritan972@gmail.com>
2024-03-09 19:10:29 +0900

    Djangoで家計簿のWebアプリケーションを作る 6　HTMLを整理する - pyhaya’s diary

--- housekeeping_book/money/views.py
+++ housekeeping_book/money/views.py
@@ -28,26 +28,35 @@ def index(request, year=None, month=None):
         total += m.cost
 
     form = SpendingForm()
-    context = {
-        'year': year,
-        'month': month,
-        'money': money,
-        'total': total,
-        'form': form
-    }
+
+    next_year, next_month = get_next(year, month)
+    prev_year, prev_month = get_prev(year, month)
+
+    context = {'year' : year,
+            'month' : month,
+            'prev_year' : prev_year,
+            'prev_month' : prev_month,
+            'next_year' : next_year,
+            'next_month' : next_month,
+            'money' : money,
+            'total' : total,
+            'form' : form
+            }
 
     draw_graph(year, month)
-    
+
     if request.method == 'POST':
         data = request.POST
         use_date = data['use_date']
         cost = data['cost']
         detail = data['detail']
         category = data['category']
+
         use_date = datetime.datetime.strptime(use_date, "%Y/%m/%d")
         tokyo_timezone = pytz.timezone('Asia/Tokyo')
         use_date = tokyo_timezone.localize(use_date)
         use_date += datetime.timedelta(hours=9)
+
         Money.objects.create(
             use_date=use_date,
             detail=detail,
@@ -55,8 +64,28 @@ def index(request, year=None, month=None):
             category=category,
         )
         return redirect(to='/money/{}/{}/'.format(year, month))
-    
+
     return render(request, 'money/index.html', context)
+
+
+def get_next(year, month):
+    year = int(year)
+    month = int(month)
+
+    if month == 12:
+        return str(year + 1), '1'
+    else:
+        return str(year), str(month + 1)
+
+
+def get_prev(year, month):
+    year = int(year)
+    month = int(month)
+    if month == 1:
+        return str(year - 1), '12'
+    else:
+        return str(year), str(month - 1)
+
 
 def draw_graph(year, month):
     money = Money.objects.filter(use_date__year=year, use_date__month=month).order_by('use_date')
