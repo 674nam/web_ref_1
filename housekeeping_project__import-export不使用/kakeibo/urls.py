999abbeb9497955442911f472b980c6aed94c9b4
674nam <moritan972@gmail.com>
2024-03-12 14:58:44 +0900

    家計簿⑥

--- housekeeping_project__import-export不使用/kakeibo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'kakeibo'
-
-urlpatterns = [
-    path('', views.PaymentList.as_view(), name='payment_list'),
-    path('income_list/', views.IncomeList.as_view(), name='income_list'),
-]
